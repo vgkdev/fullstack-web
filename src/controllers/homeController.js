8afb8af4f1b16f0d7848e506fcb390717c35b1ae
vgk <vogiakhang1507@gmail.com>
2022-08-09 00:34:53 +0700

    connected MySQL Database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,18 @@
-let getHomePage = (req, res) => {
+import db from "../models/index";
+
+let getHomePage = async (req, res) => {
   //return res.send("home controller");
-  return res.render("homepage.ejs");
-  //ko cần đường dẫn vì đã config trong file viewEngine
+  try {
+    let data = await db.User.findAll();
+    // console.log("----------------");
+    // console.log(data);
+    // console.log("----------------");
+
+    return res.render("homepage.ejs", { data: JSON.stringify(data) });
+    //ko cần đường dẫn vì đã config trong file viewEngine
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 let getAboutPage = (req, res) => {
