a148dbca6915759e342f1029f995f4cfb793c59b
vgk <vogiakhang1507@gmail.com>
2022-08-11 16:26:19 +0700

    CRUD with Nodejs - Use ORM to create new data

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
   //return res.send("home controller");
@@ -19,11 +20,20 @@ let getAboutPage = (req, res) => {
   return res.render("test/about.ejs");
 };
 
-// object: {
-//     key: '',
-//     value: ''
-// }
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  //console.log(req.body);
+  return res.send("post crud from server");
+};
+
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -6,6 +6,10 @@ let router = express.Router();
 let initWebRoutes = (app) => {
   router.get("/", homeController.getHomePage);
   router.get("/about", homeController.getAboutPage);
+  router.get("/crud", homeController.getCRUD);
+
+  router.post("/post-crud", homeController.postCRUD);
+
   // router.get("/vgk", (req, res) => {
   //   return res.send("hello world vgk");
   // });
