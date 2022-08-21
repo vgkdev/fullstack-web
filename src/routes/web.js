df126c8b57975b3b353b86dd6d81cb925c585412
vgk <vogiakhang1507@gmail.com>
2022-08-21 12:14:03 +0700

    get all users API with Nodejs

--- src/routes/web.js
+++ src/routes/web.js
@@ -16,6 +16,7 @@ let initWebRoutes = (app) => {
   router.get("/delete-crud", homeController.deleteCRUD);
 
   router.post("/api/login", userController.handleLogin);
+  router.get("/api/get-all-users", userController.handleGetAllUsers);
 
   // router.get("/vgk", (req, res) => {
   //   return res.send("hello world vgk");
